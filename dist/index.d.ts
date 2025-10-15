f15a893377de0aa66f59b9c0cd3b740f99973602
Chris <chrisbreuer93@gmail.com>
2025-10-15 13:59:28 -0700

    chore: ignore dist folder

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ActionInputs } from './types';
-
-export declare function run(): Promise<void>;
-declare function extractChangelogForVersion(changelogPath: string, version: string): string;
-declare function updateHomebrewFormula(inputs: ActionInputs,
-  assets: { name: string, browser_download_url: string }[],
-  octokit: ReturnType<typeof github.getOctokit>,): Promise<void>;
-
-export * from './types'
